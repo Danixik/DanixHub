3a2e28ee08731bb5b1aee78a496a4064f7750151
Danixik <91208187+Danixik@users.noreply.github.com>
2022-06-12 17:38:20 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 893973440 then --FLEE THE FACILITY
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Danixik/games/main/fleethefacility.lua", true))()
 elseif game.PlaceId == 6284583030 then --PSX
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Danixik/games/main/psx.lua", true))()
+elseif game.PlaceId == 9017974931 then --VALOBLOX
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Danixik/games/main/valoblox.lua", true))()
 else
     game.Players.LocalPlayer:Kick("Game not supported")
     task.wait(1)
