f37db782238741254adb610f070e3a3fa3d108cf
matt <80626082+tracebackwho@users.noreply.github.com>
2022-06-12 16:36:06 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@ if game.PlaceId == 893973440 then --FLEE THE FACILITY
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Danixik/games/main/fleethefacility.lua", true))()
 elseif game.PlaceId == 6284583030 then --PSX
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Danixik/games/main/psx.lua", true))()
-elseif game.PlaceId ~= 893973440 or 6284583030 then
-    print('| DanixHub |')
-    wait(.02)
-    print('Unsupported game, universal soon.')
+else
+    game.Players.LocalPlayer:Kick("Game not supported")
+    task.wait(1)
+    while true do end
 end
